50cd19824a283bf77f1f6f4e7fbb22ee65169df4
jayanth920 <jayanthgajula92004@gmail.com>
2022-11-13 10:21:14 -0600

    added all code in header except img src

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,3 +1,20 @@
 import React from "react";
 import "./Header.css";
 import {Link} from "react-router-dom"
+
+
+const Header = () => {
+    return(
+        <div className="header">
+            <div class="headerLeft">
+                <Link to='/'><img className="header__icon" src="" /></Link>
+                <Link to='/movies/popular'>Popular</Link>
+                <Link to='/movies/top_Rated'>Top Rated</Link>
+                <Link to='/movies/upcoming'>Upcoming</Link>
+            </div>
+        </div>
+    )
+}
+
+
+export default Header
