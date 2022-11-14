eed6fb3d7ff4b9a9a3f5436512563c05fb2f898f
jayanth920 <jayanthgajula92004@gmail.com>
2022-11-14 12:12:22 -0600

    fixed homepage

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           <Route index element={<Home />}></Route>
           <Route path="movie/:id" element={<h1> Movie Detail Page</h1>}></Route>
           <Route path="movies/:type" element={<h1> Movies List Page</h1>}></Route>
-          <Route path="/*" element={<h1> Error Page</h1>}></Route>
+          <Route path="/*" element={<Home />}></Route>
         </Routes>
       </Router>
     </div>
