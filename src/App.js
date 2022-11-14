9b579a37b3b41f431a8e5ef5073ede558c02d8e7
jayanth920 <jayanthgajula92004@gmail.com>
2022-11-14 13:08:54 -0600

    testing deploy

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
           <Route path="/" element={<Home />}></Route>
           <Route path="movie/:id" element={<h1> Movie Detail Page</h1>}></Route>
           <Route path="movies/:type" element={<h1> Movies List Page</h1>}></Route>
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </Router>
     </div>
