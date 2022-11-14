0b90b343854130da49013cc323cf98519d99ad48
jayanth920 <jayanthgajula92004@gmail.com>
2022-11-14 12:51:53 -0600

    fixed project page

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <Router>
         <Header />
         <Routes>
-          <Route index element={<Home />}></Route>
+          <Route path="/*" element={<Home />}></Route>
           <Route path="movie/:id" element={<h1> Movie Detail Page</h1>}></Route>
           <Route path="movies/:type" element={<h1> Movies List Page</h1>}></Route>
           <Route path="/*" element={<Home />}></Route>
