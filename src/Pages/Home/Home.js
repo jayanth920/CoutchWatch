a9c92770d34a93519f6aa29f40d8ac190ec934ca
jayanth920 <jayanthgajula92004@gmail.com>
2022-11-13 23:56:46 -0600

    added useeffect

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import "./Home.css"
 
 const Home = ()=>{
+
+    useEffect(() => {
+     fetch(`https://api.themoviedb.org/3/movie/popular?api_key=9b857e73ed1b3ac73813003343300b97&language=en-US&page=1`)
+     .then(res => res.json())
+     .then(data => console.log(data))
+    }, [])
+    
+
+
     return(
         <>
         Home Page
