d09413caeee15f4760a4e06fe5c2af6c2b36e514
jayanth920 <jayanthgajula92004@gmail.com>
2022-11-14 13:12:24 -0600

    testing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import Header from './components/Header/Header'
-import Home  from './Pages/Home/Home';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Header from './components/header/Header';
+import Home from './pages/home/home';
+import MovieList from './components/movieList/movieList';
+import Movie from './pages/movieDetail/movie';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="movie/:id" element={<h1> Movie Detail Page</h1>}></Route>
-          <Route path="movies/:type" element={<h1> Movies List Page</h1>}></Route>
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </Router>
+        <Router>
+          <Header />
+            <Routes>
+                <Route index element={<Home />}></Route>
+                <Route path="movie/:id" element={<Movie />}></Route>
+                <Route path="movies/:type" element={<MovieList />}></Route>
+                <Route path="/*" element={<h1>Error Page</h1>}></Route>
+            </Routes>
+        </Router>
     </div>
   );
 }
