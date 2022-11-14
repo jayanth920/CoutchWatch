12ff6ca1c90a63b6338a3dff1820d443bc02d660
jayanth920 <jayanthgajula92004@gmail.com>
2022-11-14 09:09:10 -0600

    added movielist js and css , not coded anything yet

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,5 +1,8 @@
 import React, {useState,useEffect} from 'react';
 import "./Home.css"
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import { Link } from "react-router-dom";
 
 const Home = ()=>{
     const [ popularMovies , setPopularMovies] = useState([])
@@ -15,10 +18,39 @@ const Home = ()=>{
 
     return(
 
-        <div>{popularMovies.map(movie =>( 
-        <span>{movie.original_title}</span>
-        ))
-        }</div>
+        <>
+        <div className="poster">
+            <Carousel
+                showThumbs={false}
+                autoPlay={true}
+                transitionTime={3}
+                infiniteLoop={true}
+                showStatus={false}
+            >
+                {
+                    popularMovies.map(movie => (
+                        <Link style={{textDecoration:"none",color:"white"}} to={`/movie/${movie.id}`} >
+                            <div className="posterImage">
+                                <img src={`https://image.tmdb.org/t/p/original${movie && movie.backdrop_path}`} />
+                            </div>
+                            <div className="posterImage__overlay">
+                                <div className="posterImage__title">{movie ? movie.original_title: ""}</div>
+                                <div className="posterImage__runtime">
+                                    {movie ? movie.release_date : ""}
+                                    <span className="posterImage__rating">
+                                        {movie ? movie.vote_average :""}
+                                        <i className="fas fa-star" />{" "}
+                                    </span>
+                                </div>
+                                <div className="posterImage__description">{movie ? movie.overview : ""}</div>
+                            </div>
+                        </Link>
+                    ))
+                }
+            </Carousel>
+            <MovieList />
+        </div>
+    </>
     )
 }
  export default Home
