f1845772dcf2dd2abd1500939c039cecdcdd325c
jayanth920 <jayanthgajula92004@gmail.com>
2022-11-14 15:28:13 -0600

    solving links

--- src/App.js
+++ src/App.js
@@ -11,10 +11,9 @@ function App() {
         <Router>
           <Header />
             <Routes>
-            <Route path="/" element={<Home />}></Route>
+            <Route path="/*" element={<Home />}></Route>
                 <Route path="movie/:id" element={<Movie />}></Route>
                 <Route path="movies/:type" element={<MovieList />}></Route>
-                <Route path="*" element={<Home />}></Route>
             </Routes>
         </Router>
     </div>
