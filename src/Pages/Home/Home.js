12b5e273ff6befd16514c3b0ed493119158af546
jayanth920 <jayanthgajula92004@gmail.com>
2022-11-14 00:16:40 -0600

    added url api, working popular api

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,19 +2,23 @@ import React, {useState,useEffect} from 'react';
 import "./Home.css"
 
 const Home = ()=>{
+    const [ popularMovies , setPopularMovies] = useState([])
 
     useEffect(() => {
      fetch(`https://api.themoviedb.org/3/movie/popular?api_key=9b857e73ed1b3ac73813003343300b97&language=en-US&page=1`)
      .then(res => res.json())
-     .then(data => console.log(data))
+     .then(boss=>console.log(boss.results))
+     .then(data => setPopularMovies(data.results))
     }, [])
     
 
 
     return(
-        <>
-        Home Page
-        </>
+
+        <div>{popularMovies.map(movie =>( 
+        <span>{movie.original_title}</span>
+        ))
+        }</div>
     )
 }
  export default Home
